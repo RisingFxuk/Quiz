d41af6e496dbe9f0db6d91d2d6d848f445647460
MiquelVidal <a2308927@institutmontilivi.cat>
2024-01-31 20:06:56 +0100

    Miquel'sChanges

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello Montilivi!");
             Console.WriteLine("I'm learning Git, woo!");
+            Console.WriteLine("This is a comment by Miquel Vidal on Glen's code");
         }
     }
 }
